80884aca1fe3069a6bb432e7b53df1d9a03a32cd
Erik Horton <hortonew@gmail.com>
2025-08-09 09:37:44 -0600

    Working setup

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,16 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::Manager;
+
+#[tauri::command]
+fn say_hello(name: String) -> String {
+    format!("Hello, {name} 👋 (from Rust)")
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![say_hello])
+        .run(tauri::generate_context!())
+        .expect("error running app");
+}
